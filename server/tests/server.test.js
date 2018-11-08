9b3c18103cf9b6b1d2dfdb364dbed6659bf977a3
Michael Evans <azmathtech@gmail.com>
2018-11-07 21:11:26 -0700

    Add tests for PATCH/todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -12,7 +12,9 @@ const todos = [
   },
   {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
   }
 ];
 
@@ -140,5 +142,47 @@ describe('DELETE /todos/:id', () => {
 
   it('should return 404 if object id is invalid', done => {
     request(app).delete('/todos/123').expect(404).end(done);
+  });
+});
+
+describe('PATCH /todos/:id', () => {
+  it('should update the todo', done => {
+    var hexId = todos[0]._id.toHexString();
+    var text = 'test update';
+    var completed = true;
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({ text, completed })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed', done => {
+    var hexId = todos[1]._id.toHexString();
+    var text = 'test update 2';
+    var completed = false;
+    // var completedAt = null;
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({ text, completed })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo.text).toBe(text);
+      })
+      .expect(res => {
+        expect(res.body.todo.completed).toBe(false);
+      })
+      .expect(res => {
+        //expect(res.body.todo.completedAt).toBe(null);
+        expect(res.body.todo.completedAt).toNotExist();
+      })
+      .end(done);
   });
 });
