b3f47c3777d2c0b952a8547ce0ae989a11878f91
Michael Evans <azmathtech@gmail.com>
2018-11-09 17:33:02 -0700

    Add Get/users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -60,6 +60,27 @@ UserSchema.methods.generateAuthToken = function() {
   });
 };
 
+//statics is a model method rather than an instance method
+UserSchema.statics.findByToken = function(token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    // return new Promise((resolve, reject) => {
+    //   reject()
+    // })
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    _id: decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -143,6 +144,28 @@ app.post('/users', (req, res) => {
 
   console.log(user);
 });
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
+//used to test to see if a user is validated (pre-refactor)
+// app.get('/users/me', authenticate, (req, res) => {
+//   var token = req.header('x-auth');
+//
+//   User.findByToken(token)
+//     .then(user => {
+//       if (!user) {
+//         // res.status(401).send();
+//         return Promise.reject();
+//       }
+//
+//       res.send(user);
+//     })
+//     .catch(e => {
+//       res.status(401).send();
+//     });
+// });
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
