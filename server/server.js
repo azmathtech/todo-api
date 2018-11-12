479e2e130145f2ac84d18f6317df5701e827fc70
Michael Evans <azmathtech@gmail.com>
2018-11-12 14:19:59 -0700

    Associate users with todos

--- server/server.js
+++ server/server.js
@@ -17,9 +17,10 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json()); //middleware
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    _creator: req.user._id
   });
 
   todo.save().then(
@@ -34,8 +35,10 @@ app.post('/todos', (req, res) => {
   console.log(req.body);
 });
 
-app.get('/todos', (req, res) => {
-  Todo.find().then(
+app.get('/todos', authenticate, (req, res) => {
+  Todo.find({
+    _creator: req.user._id
+  }).then(
     todos => {
       res.send({ todos });
     },
@@ -45,7 +48,7 @@ app.get('/todos', (req, res) => {
   );
 });
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
@@ -53,7 +56,11 @@ app.get('/todos/:id', (req, res) => {
     // console.log('ID not valid');
   }
 
-  Todo.findById(id)
+  // Todo.findById(id)
+  Todo.findOne({
+    _id: id,
+    _creator: req.user._id
+  })
     .then(
       todo => {
         if (!todo) {
@@ -72,14 +79,18 @@ app.get('/todos/:id', (req, res) => {
   //res.send(req.params);
 });
 
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-  Todo.findByIdAndRemove(id)
+  // Todo.findByIdAndRemove(id)
+  Todo.findOneAndRemove({
+    _id: id,
+    _creator: req.user._id
+  })
     .then(todo => {
       if (!todo) {
         return res.status(404).send();
@@ -93,7 +104,7 @@ app.delete('/todos/:id', (req, res) => {
     });
 });
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
 
@@ -108,7 +119,15 @@ app.patch('/todos/:id', (req, res) => {
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+  // Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+  Todo.findOneAndUpdate(
+    {
+      _id: id,
+      _creator: req.user._id
+    },
+    { $set: body },
+    { new: true }
+  )
     .then(todo => {
       if (!todo) {
         return res.status(404).send();
